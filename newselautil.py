dbe8b46ca6882a01ce877a57c3b99b7361366de1
Dargones <alexanderfedchin@Alexanders-MacBook-Air.local>
2018-06-05 17:17:51 -0400

    new log file

--- newselautil.py
+++ newselautil.py
@@ -1,8 +1,5 @@
 # Utils for newsela articles
-# The main collects article stats to be combined with parse information.
-# You must run the tokenizer on all files you want to
-# process prior to using main.
-# Au: S. Anderson
+# Author: S. Anderson
 # Modified: A. Fedchin
 
 import io
@@ -11,19 +8,17 @@ import string
 import csv
 import nltk.data
 import regex as re
+import classpaths as path
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 
 from nltk.corpus import stopwords
 STOPWORDS = stopwords.words('english')
-STOPWORDS.append("`s")
+STOPWORDS.append("`s")  # TODO: Should this really be appended?
 STOPWORDS.append("n`t")
-length = len(STOPWORDS)-2
-for i in range(length):
-    wordBeginningWithCapital = STOPWORDS[i][0].capitalize() + STOPWORDS[i][1:]
-    STOPWORDS.append(wordBeginningWithCapital)
-import classpaths as path
+for i in range(len(STOPWORDS) - 2):
+    STOPWORDS.append(STOPWORDS[i][0].capitalize() + STOPWORDS[i][1:])
 
 HDR = ['title', 'filename', 'grade_level', 'language',  'version', 'slug']
 Tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -44,7 +39,6 @@ def loadMetafile():
             if row['language'] == 'en':
                 enArticles += 1
                 articles.append(row)
-    # print 'English %d Total %d' % (enArticles,numArticles)
     return articles
 
 
@@ -56,7 +50,6 @@ def cleanSentence(s):
 
 def cleanSentences(slist):
     """Strip material from list of sentences."""
-    DASH = u"\u2014"
     sentlist = []
     for s in slist:
         s1 = cleanSentence(s)
@@ -66,17 +59,13 @@ def cleanSentences(slist):
     return sentlist
 
 
-# Some articles begin with location name that is written in all caps. The
-# location has something to do with the topic of the article, but it is appended
-# to any sentence that happens to be the first one. It should not be taken into
-# account during the alignment. However, some articles begin with other
-# capitalized words that should be taken into account. Here is the list of these
-CAPITALIZED_WORDS = ['A', '``', 'U.S.', 'C.J.', 'U.K.', 'FBI', 'FDA', '###', '_',
-                    'NASA', 'DNA', 'TV', 'UFO', 'B.B.', '!\n']
-LINES_TO_IGNORE = ["## Preamble\n", "CUPERTINO , Calif. .\n",
-                   "CHARLESTON , W.Va .\n", "PORTLAND , Ore. .\n",
-                   "KANSAS CITY , Missouri .\n", "WICHITA , Kan. .\n",
-                   "CHICAGO _ As politicians in Washington took a step toward tightening the nation `s gun laws on Wednesday , first lady Michelle Obama sat down with Chicago high school students whose stories about violence brought her to tears .\n"]
+# Some articles begin with the name of the city in which the event discussed
+# has happened. These "location tags" should not affect alignment and should
+# be removed. Here are some abbreviations with which some lines begin but
+# which should not be removed.
+CAPITALIZED_WORDS = ['A' 'FBI', 'FDA', 'NASA', 'DNA', 'TV', 'UFO']
+OTHER_HEADERS = '(SEATTLE - )'
+# these are headers that are different to detect otherwise
 
 
 def modify_the_header(line):
@@ -85,41 +74,18 @@ def modify_the_header(line):
     :param line: the line to modify
     :return: the modified line
     """
-    # print(line + "\n")
-    # isUpperCase = True
-    if line not in LINES_TO_IGNORE:
-        re.sub('### PRO : ', '', line)
-        re.sub('<.*> ', '', line)
-        re.sub('### PRO : ', '', line)
-        if line.split(' ')[0] not in CAPITALIZED_WORDS:
-            re.sub('^[A-Z][A-Z]* ([^-].?|.?[^-])*[^-]*-- ', '', line)
-    """firstWord = line.split(' ')[0]
-    for letter in firstWord:
-        if letter != letter.upper():
-            isUpperCase = False
-    if isUpperCase and firstWord not in CAPITALIZED_WORDS:
-        i = 0
-        while ((i < len(line)) and (line[i] != "-")) or ((i + 1 < len(line)) and (line[i+1] == "-")):
-            i += 1
-        print(line[:i + 2])
-        line = line[i+2:]
-    elif firstWord == "###":  # ### PRO :
-        print(line[:10])
-        line = line[10:]
-    elif firstWord == '_':
-        print(line[:2])
-        line = line[2:]
-    elif firstWord == "!\n":  # a header with an image
-        # TODO
-        pass
-    if line[0] == '<':  # another version of a header with an image
-        i = 0
-        while (i < len(line)) and (line[i] != '>'):
-            i += 1
-        if i+2 < len(line):
-            print(line[:i + 2])
-            line = line[i + 2:]
-            return modify_the_header(line)"""
+    length = len(line)
+    oldline = line
+    line = re.sub('### PRO : ', '', line)
+    line = re.sub('<.*> ', '', line)
+    line = re.sub('### PRO : ', '', line)
+    if line.split(' ')[0] not in CAPITALIZED_WORDS:
+        line = re.sub('^[A-Z][A-Z\-]* .*?-- ', '', line)
+        line = re.sub('^' + OTHER_HEADERS, '', line)
+    if length != len(line):
+        print(oldline[:length-len(line)])
+    # firstWord == "!\n":  # a header with an image
+    # TODO
     return line
 
 
@@ -137,7 +103,8 @@ def getTokParagraphs(article, separateBySemicolon=True, MODIFY_HEADER=True):
     PARPREFIX = "@PGPH "  # Delimits paragraphs in FILE.tok
     pars = []
     slist = []
-    with io.open(path.BASEDIR + '/articles/' + article['filename']+SUFFIX, mode='r', encoding='utf-8') as fd:
+    with io.open(path.BASEDIR + '/articles/' + article['filename']+SUFFIX,
+                 mode='r', encoding='utf-8') as fd:
         lines = fd.readlines()
         if MODIFY_HEADER:
             lines[1] = modify_the_header(lines[1])
@@ -179,14 +146,11 @@ def convertPOS(pos):
     return None
 
 
-def removeCaps(lst):
-    return [x for x in lst if x.lower() == x]
-
-
 def lemmatize(s):
     """Return list of lemmas for string s, a sentence."""
     tokens = Wordtokenizer.tokenize(s)
-    # tokens = removeCaps(tokens) # remove any string with uppercase char
+    # tokens = [x for x in tokens if x.lower() == x]
+    # remove any string with uppercase char
     # (eg, proper names)
     cleantokens = []
     for w in tokens:
