4bed7818708ecfaefcbd51746528855b57abe2cc
Dargones <sasha.fedchin@gmail.com>
2018-06-06 14:08:13 -0400

    modified classpaths

--- classpaths.py
+++ classpaths.py
@@ -1,12 +1,11 @@
-# Contains all the path variables that need to be configured before running
-# the program on a new computer
+# Contains all the path variables that need to be configured before running the program on a new computer
+# 
 
 import sys
 
-USERDIR = '/home/af9562'
-if sys.platform == 'darwin':
-    USERDIR = '/Users/alexanderfedchin'
-BASEDIR = USERDIR + '/newsela'
+USERDIR = '/home/nlp'
+if sys.platform == 'darwin': USERDIR = '/Users/alexanderfedchin'
+BASEDIR = USERDIR + '/alignment'
 METAFILE = BASEDIR + '/articles_metadata.csv'
 PARSERDIR = BASEDIR + '/stanford-parser-full-2015-12-09/'
 OUTDIR_SENTENCES = BASEDIR+'/output/sentences/'
@@ -22,5 +21,5 @@ MANUAL_PARAGRAPHS = BASEDIR+'/manual/paragraphs/'
 PARSERPROG = 'custom/Parser'
 TOKENIZERPROG = 'custom/Tokenizer'
 
-CLASSPATH = ':'.join(['.',PARSERDIR,PARSERDIR + 'stanford-parser.jar', PARSERDIR + 'stanford-parser-3.6.0-models.jar', PARSERDIR + 'slf4j-api.jar'])
+CLASSPATH = ':'.join(['.',PARSERDIR,PARSERDIR + 'stanford-parser.jar',PARSERDIR + 'stanford-parser-3.6.0-models.jar', PARSERDIR + 'slf4j-api.jar'])
 
