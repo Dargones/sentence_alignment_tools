f09504e97188a555b55df265b0d13b5ea752c6b0
Dargones <alexanderfedchin@Alexanders-MacBook-Air.local>
2018-06-06 14:13:04 -0400

    Making the code run on both computers

--- classpaths.py
+++ classpaths.py
@@ -5,7 +5,7 @@ import sys
 
 USERDIR = '/home/nlp'
 if sys.platform == 'darwin': USERDIR = '/Users/alexanderfedchin'
-BASEDIR = USERDIR + '/alignment'
+BASEDIR = USERDIR + '/newsela'
 METAFILE = BASEDIR + '/articles_metadata.csv'
 PARSERDIR = BASEDIR + '/stanford-parser-full-2015-12-09/'
 OUTDIR_SENTENCES = BASEDIR+'/output/sentences/'
